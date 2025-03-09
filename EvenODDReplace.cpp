8100df34656b46fb77936b4c9206d076050ca097
MR. Morningstar <ee8991471@gmail.com>
2025-03-09 20:39:23 +0600

    Added

--- EvenODDReplace.cpp
+++ EvenODDReplace.cpp
@@ -2,9 +2,31 @@
 using namespace std;
 int main()
 {
-    int size;
-    cout<<"Enter the size of the array: "; cin>>size;
+    int n;
+    cout<<"Enter the size of the array: "; cin>>n;
     int arr[n];
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+
+    }
+
+    cout<<"------------------------------------------"<<endl;
+
+    for(int i=0; i<n; i++)
+    {
+            if(arr[i]%2 == 0)
+        {
+            cout<<0<<endl;
+        }else
+        {
+            cout<<1<<endl;
+        }
+
+
+    }
+
 
 
 
